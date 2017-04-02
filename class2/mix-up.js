16779f1ea0f6be104d2057cc306e45bba35d68c2
MD <codepro.md@gmail.com>
2017-04-01 23:13:47 -0700

    Update Mix Up functionality using only slice

--- class2/mix-up.js
+++ class2/mix-up.js
@@ -11,11 +11,20 @@ function mixUp (str1, str2) {
 	secondTwo = str2.slice(0,2);
 	secondLast = str2.substring(2,str2.length);
 	mixup = secondTwo.concat(firstLast, ' ', firstTwo, secondLast);
-	document.write('(' + str1 + ', ' + str2 +'): ' + mixup + '<br>');
+	document.write('(' + str1 + ', ' + str2 +'):<br>' + mixup + ' \"answer using slice & substring methods\"<br>');
 
 }
 
+function sliced (str1, str2) {
+	document.write(str2.slice(0,2) + str1.slice(2) + " " +
+	str1.slice(0,2) + str2.slice(2) +" \"answer using only slice method\"<br>");
+}
+
 mixUp('mix', 'pod');
+sliced('mix', 'pod');
 mixUp('dog', 'dinner');
+sliced('dog', 'dinner');
 mixUp('assume', 'asign');
+sliced('assume', 'asign');
 mixUp('dig', 'donner');
+sliced('dig', 'donner');
