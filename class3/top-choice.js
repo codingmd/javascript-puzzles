c904976a1aa34bce275e81e9aa1993b2cea00f99
MD <codepro.md@gmail.com>
2017-04-03 23:25:28 -0700

    Update Top Choice task: correct part 1 and include bonus

--- class3/top-choice.js
+++ class3/top-choice.js
@@ -4,31 +4,77 @@
 // Bonus: Change it to log "My 1st choice", "My 2nd choice", "My 3rd choice", 
 // picking the right suffix for the number based on what it is.
 
-var topChoice = {
+topChoice = {
 	favoriteColors: ['blue', 'pink', 'yellow'],
 	favoriteTvGenres: ['sci fi', 'drama', 'comedy','fantasy'],
 	favoriteDecades: [80, 90]
 }
 
+document.write(("Choice with #number identifier<br>").toUpperCase() + '<br>');
 index = 0;
 for (var i in topChoice) {
+	element = 0;
 	document.write(Object.keys(topChoice)[index] + '<br>');
 	if (topChoice.hasOwnProperty(i)) {
-		var element = 0;
 		// document.write(Object.keys(topChoice)[element] + '<br>');
-		while (topChoice[i].length > 0 && element < 3) {
+		while (topChoice[i].length) {
 			if (element == 0) {
-				document.write('My 1st choice: ' + topChoice[i].shift() + '<br>');
+				document.write('My #' + (element + 1) + ' choice: ' + topChoice[i].shift() + '<br>');
 				element++;
 			}
 			else if (element == 1) {
-				document.write('My 2nd choice: ' + topChoice[i].shift() + '<br>');
+				document.write('My #' + (element + 1) + ' choice: ' + topChoice[i].shift() + '<br>');
 				element++;
 			}
 			else if (element == 2) {
-				document.write('My 3rd choice: ' + topChoice[i].shift() + '<br>');
+				document.write('My #' + (element + 1) + ' choice: ' + topChoice[i].shift() + '<br>');
+				element++;
+			}
+			else {
+				document.write('My #' + (element + 1) + ' choice: ' + topChoice[i].shift() + '<br>');
 				element++;
 			}	
+		}
+	}
+	document.write('<br>');
+	index++;
+}
+
+document.write(('Bonus: Choice with suffix identifier<br>').toUpperCase() + '<br>');
+//using element check to determine correct # suffix for statement
+topChoice = {
+	favoriteColors: ['blue', 'pink', 'yellow'],
+	favoriteTvGenres: ['sci fi', 'drama', 'comedy','fantasy'],
+	favoriteDecades: [80, 90]
+}
+
+suffix = ['st', 'nd', 'rd', 'th'];
+index = 0;
+for (var j in topChoice) {
+	element = 0;
+	document.write(Object.keys(topChoice)[index] + '<br>');
+	if (topChoice.hasOwnProperty(j)) {
+		while (topChoice[j].length > 0) {
+			if (element == 0) {
+				document.write('My ' + (element + 1)+ suffix[element] + ' choice: ' + 
+					topChoice[j].shift() + '<br>');
+				element++;
+			}
+			else if (element == 1) {
+				document.write('My ' + (element + 1) + suffix[element] + ' choice: ' + 
+					topChoice[j].shift() + '<br>');
+				element++;
+			}
+			else if (element == 2) {
+				document.write('My ' + (element + 1) + suffix[element] + ' choice: ' + 
+					topChoice[j].shift() + '<br>');
+				element++;
+			}
+			else {
+				document.write('My ' + (element + 1) + suffix[3] + ' choice: ' + 
+					topChoice[j].shift() + '<br>');
+				element++;
+			}
 		}
 	}
 	document.write('<br>');
